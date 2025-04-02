6d73b986ec253544c9c6fd284cdfeb2193e54410
Tristan Rosales <tristanrosales0@gmail.com>
2025-04-02 18:24:11 +0800

    Disable toast for getAllClinicSession hook

--- src/endpoints/clinic-session.ts
+++ src/endpoints/clinic-session.ts
@@ -1,6 +1,9 @@
 import axios from 'axios';
 
-export const getAllClinicSession = () => axios.get('/api/v1/customers/clinic_sessions');
+export const getAllClinicSession = () => {
+    const config: any = { disableToast: true };
+    return axios.get('/api/v1/customers/clinic_sessions', config)
+};
 
 export const getClinicSessionByRefNo = (refno: string | undefined | null) => {
     const config: any = { disableToast: true };
